e56ab8e8e66740e149c69c0d3c7980c6f04356ec
melissawebster <imelissawebster@gmail.com>
2025-04-09 17:17:02 -0300

    update buttons 2

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -8,7 +8,24 @@ interface ButtonProps {
 
 export const GreenButton = (props: ButtonProps) => (
   <Button 
-    className={twMerge("bg-green-400 text-white transition-colors", props.className)}
+    className={twMerge("bg-green-500 hover:bg-green-600 text-white transition-colors cursor-pointer", props.className)}
+  >
+    {props.children}
+  </Button>
+)
+
+export const BlackButton = (props: ButtonProps) => (
+  <Button 
+    className={twMerge("bg-black text-green-500 transition-colors cursor-pointer border border-green-500", props.className)}
+  >
+    {props.children}
+  </Button>
+)
+
+export const GhostButton = (props: ButtonProps) => (
+  <Button 
+    className={twMerge("text-green-500 hover:bg-transparent hover:text-green-300 transition-colors cursor-pointer", props.className)}
+    variant="ghost"
   >
     {props.children}
   </Button>
