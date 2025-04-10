839a84b927312b39f5d5320325cf4b56c01f9474
melissawebster <imelissawebster@gmail.com>
2025-04-10 16:34:04 -0300

    create footer skeleton

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+
+export default function Footer() {
+  return (
+    <footer
+      className="inset-x-0 bottom-0 bg-black flex justify-center py-4 border-t-1 h-20"
+    >
+        Test
+    </footer>
+  );
+}
