d296f29235fdf52454778ffef489dfc548743e6c
melissawebster <imelissawebster@gmail.com>
2025-04-11 17:45:21 -0300

    update title

--- src/components/text-styles.tsx
+++ src/components/text-styles.tsx
@@ -1,7 +1,8 @@
 import { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-export const BlackTitle = ({ children }: { children: ReactNode }) => (
-  <h1 className="text-black font-bold text-xl">
+export const BlackTitle = ({ children, className }: { children: ReactNode, className?: string }) => (
+  <div className={twMerge(`text-black font-bold text-xl`, className)}>
     {children}
-  </h1>
+  </div>
 );
