eb0b1de2e0ef7a47fcd512fd7d3de006d9710074
melissawebster <imelissawebster@gmail.com>
2025-04-16 12:03:50 -0300

    define footer height

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,7 @@ import Image from "next/image"
 export default function Footer() {
   return (
     <footer
-      className="inset-x-0 bottom-0 bg-black flex justify-center py-4 border-t-1 h-fit"
+      className="inset-x-0 bottom-0 bg-black flex justify-center py-4 border-t-1 h-[460px] md:h-[180px]"
     >
       <div className="flex flex-col gap-y-6 md:flex-row text-white w-fit md:w-[90%] md:justify-between lg:w-[70%] my-4">
 
