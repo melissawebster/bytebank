a3803a9999e287a21260938f18ee10db5a9d9b15
melissawebster <imelissawebster@gmail.com>
2025-05-30 20:12:49 -0300

    add links to menu

--- src/components/dashboard/nav.tsx
+++ src/components/dashboard/nav.tsx
@@ -1,4 +1,5 @@
 import { twMerge } from "tailwind-merge";
+import Link from "next/link";
 
 interface NavDashboardProps {
   type: 'tablet' | 'desktop';
@@ -6,20 +7,37 @@ interface NavDashboardProps {
 }
 
 export default function NavDashboard({type, className} : NavDashboardProps) {
-  const menuItems = ['Início', 'Transferências', 'Investimentos', 'Outros serviços']
+  const menuItems = [
+    {nome: 'Início', url:'/dashboard'},
+    {nome: 'Transferências', url:'/dashboard'},
+    {nome: 'Investimentos', url:'/dashboard/meus-investimentos'},
+    {nome: 'Outros serviços', url:'/dashboard/servicos'},
+  ]
   return (
     <div className={twMerge('w-full', className)}>
       {type === 'tablet'  && (
         <div className="w-full flex flex-row justify-between px-4">
           {menuItems.map((item, index) => (
-            <div className="p-4" key={index}>{item}</div>
+            <Link 
+              href={item.url} 
+            >
+              <div className="p-4" key={index}>
+                {item.nome}
+              </div>
+            </Link>
           ))}
         </div>
       )}
       {type === 'desktop'  && (
         <div className="w-full h-full flex-col p-4 bg-white rounded-md">
           {menuItems.map((item, index) => (
-            <div className="p-4 text-sm border-b border-green-300" key={index}>{item}</div>
+            <Link 
+              href={item.url} 
+            >
+              <div className="p-4" key={index}>
+                {item.nome}
+              </div>
+            </Link>
           ))}
         </div>
       )}
