de8b2832826182af0da840846c8c0581e4806c0a
melissawebster <imelissawebster@gmail.com>
2025-09-07 22:08:49 -0300

    create shared components file

---
+++ src/components/shared.tsx
@@ -0,0 +1,19 @@
+  import Image from "next/image"
+  
+  export const FullLogo = () => {
+    return (
+      <>
+        <Image
+          src={"/assets/logo-bytebank.png"}
+          alt="Bytebank Logo"
+          width={90}
+          height={40}
+          priority
+        />
+        <div className="flex text-white">
+          <div className="font-electrolize neon-turquoise text-3xl">Byte</div>
+          <div className="font-electrolize lime-green text-3xl">Bank</div>
+        </div>
+      </>
+    )
+  }
