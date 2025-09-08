f25c3e119094f30ece37f70aa43da61d3e9856a8
melissawebster <imelissawebster@gmail.com>
2025-09-08 13:33:37 -0300

    update footer and white logo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,31 +4,30 @@ import Image from "next/image"
 
 export default function Footer() {
   return (
-    <footer className="bg-black flex justify-center py-4 border-t">
+    <footer className="bg-dark-transp flex justify-center py-4 border-t">
 
       <div className="container mx-auto flex flex-col gap-y-6 md:flex-row text-white md:justify-between my-4 px-4 sm:px-6 md:px-12 lg:px-16 xl:px-20">
 
         <div className="flex flex-col gap-y-2">
-          <div className="font-bold">Serviços</div>
-          <div>Conta corrente</div>
-          <div>Conta PJ</div>
-          <div>Cartão de crédito</div>
+          <div className="font-bold lime-green">Services</div>
+          <div>Checking account</div>
+          <div>Salary account</div>
+          <div>Credit card</div>
         </div>
 
         <div className="flex flex-col gap-y-2">
-          <div className="font-bold">Contato</div>
+          <div className="font-bold lime-green">Contact</div>
           <div>0800 004 250 08</div>
-          <div>meajuda@bytebank.com.br</div>
-          <div>ouvidoria@bytebank.com.br</div>
+          <div>help@bytebank.com</div>
         </div>
 
         <div className="flex flex-col gap-y-4">
-          <div className="font-bold">Desenvolvido por Alura</div>
+          <div className="font-bold lime-green">Project for FIAP</div>
           <Image 
-            src={'/assets/logo-bytebank-white.png'}
-            width={146}  
-            height={33} 
-            alt="logo bytebank" 
+            src={'/assets/logo-bytebank-white.svg'}
+            width={176}  
+            height={37} 
+            alt="logo bytebank"
           />
           <div className="flex gap-x-3">
             <Image 
