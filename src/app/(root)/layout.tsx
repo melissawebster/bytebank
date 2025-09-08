0eadcf8d8b5d4aaa8c79aa4bc23182b150a9c048
melissawebster <imelissawebster@gmail.com>
2025-09-08 13:16:54 -0300

    add home illustration and style hero section

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -7,9 +7,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen bg-[linear-gradient(to_bottom,#004D61_0%,#FFF_100%)]">
+    <div className="flex flex-col min-h-screen bg-[linear-gradient(to_bottom,#554683,#6EEFFD)]">
       <Header />
-      <main className="bg-container mx-auto mt-[5rem] min-h-[calc(100vh-60px-4rem)] px-4 sm:px-6 md:px-12 lg:px-16 xl:px-20">
+      <main className="mx-auto mt-[5rem] min-h-[calc(100vh-60px-4rem)]">
         {children}
       </main>
       <Footer />
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -19,22 +19,24 @@ export default function Home() {
     const [isLoginOpen, setIsLoginOpen] = useState(false)
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex flex-col lg:flex-row md:items-center lg:gap-x-8 lg:w-[85%]">
-        <BlackTitle className="w-full md:w-3/4 text-center mt-4 lg:mt-0 ">
-          <p>
-            Experimente mais liberdade no controle da sua vida financeira.
-          </p> 
-          <p>
-            Crie sua conta com a gente!
-          </p>
-        </BlackTitle>
-        <Image
-          src={ilustracao}
-          alt="Ilustracao"
-          className="mt-8"
-        />
+    <div className="container flex flex-col items-center px-4 md:px-12 lg:px-16 xl:px-20">
+      {/* Hero Section */}
+      <div className="flex flex-col md:flex-row md:items-center lg:gap-x-4 w-full border rounded-lg bg-black/25 p-4">
+        <div className="w-full md:w-3/4 text-center mt-4 md:mt-0 text-2xl sm:text-4xl md:text-3xl lg:text-4xl xl:text-5xl lime-green opacity-80 
+                        font-electrolize leading-snug drop-shadow-[0_0_3px_rgba(0,0,0,0.3)]">
+          Experience&nbsp;greater&nbsp;freedom<br />in&nbsp;your&nbsp;financial&nbsp;life
+        </div>
+        <div className="flex-1 flex justify-center">
+          <Image
+            src="/assets/credit-cards.svg"
+            width={428}
+            height={280}
+            alt="credit"
+            className="max-w-full object-contain"
+          />
+        </div>
       </div>
+      {/* */}
 
       <div className="w-full flex justify-between mt-4 gap-x-4 md:hidden">
 
