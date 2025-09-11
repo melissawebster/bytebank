816936bd63fe67cdca9d5bfa2a6d2a86f21b4181
melissawebster <imelissawebster@gmail.com>
2025-09-11 14:44:40 -0300

    change text

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div className="container flex flex-col items-center px-4 md:px-12 lg:px-16 xl:px-20">
       {/* Hero Section */}
-      <div className="flex flex-col md:flex-row md:items-center lg:gap-x-4 w-full border rounded-lg bg-black/25 p-4">
+      <div className="flex flex-col md:flex-row md:items-center lg:gap-x-4 w-full border border-0.2 rounded-lg bg-black/25 p-4">
         <div className="w-full md:w-3/4 text-center mt-4 md:mt-0 text-2xl sm:text-4xl md:text-3xl lg:text-4xl xl:text-5xl lime-green opacity-80 
                         font-electrolize leading-snug drop-shadow-[0_0_3px_rgba(0,0,0,0.3)]">
           Experience&nbsp;greater&nbsp;freedom<br />in&nbsp;your&nbsp;financial&nbsp;life
@@ -56,20 +56,20 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col items-center">
-        <BlackTitle className="mt-12 text-white drop-shadow-[0_0_5px_rgba(0,0,0,0.5)]">
-          We offer
+        <BlackTitle className="mt-12 text-white font-extralight text-lg drop-shadow-[0_0_5px_rgba(0,0,0,0.5)]">
+          Exclusive Advantages
         </BlackTitle>
-        <div className="flex flex-col lg:flex-row gap-y-10 lg:gap-x-10 my-10 max-w-3/4 md:max-w-full">
+        <div className="flex flex-col lg:flex-row gap-y-10 lg:gap-x-10 mb-10 mt-5 max-w-3/4 md:max-w-full">
           <div className="flex flex-col md:flex-row gap-y-10 md:gap-x-10">
             <CardLanding
               img={<Image src='/assets/icon-card.svg' width={80} height={80} alt="Free account & card" />}
               title="Free account & card"
-              description="Our account is digital, with no fixed fees - and, most importantly: no monthly maintenance fee."
+              description="A free digital account. No monthly fees, ever."
             />
             <CardLanding
               img={<Image src='/assets/icon-money.svg' width={80} height={80} alt="Free cash withdrawals" />}
               title="Free cash withdrawals"
-              description="You can withdraw cash for free 4 times a month at any ATM nationwide."
+              description="You can withdraw cash for free 4 times a month at any ATM."
             />
           </div>
           <div className="flex flex-col md:flex-row gap-y-10 md:gap-x-10">
@@ -81,7 +81,7 @@ export default function Home() {
             <CardLanding
               img={<Image src='/assets/icon-security.svg' width={80} height={80} alt="Device Insurance" />}
               title="Device Insurance"
-              description="Your mobile devices (smartphone and laptop) are protected for a small symbolic fee."
+              description="Your mobile devices are protected for a small symbolic fee."
             />
           </div>
         </div>
--- src/components/card-landing.tsx
+++ src/components/card-landing.tsx
@@ -9,9 +9,15 @@ interface CardLangingProps {
 export default function CardLanding({ img, title, description }: CardLangingProps) {
 	return (
 		<div className="flex flex-col items-center w-full md:w-1/2 border p-4 rounded-lg bg-black/25">
-			<div>{img}</div>
-			<div className="lime-green font-electrolize mt-2">{title}</div>
-			<div className="text-white text-center font-light text-sm leading-relaxed mt-3">{description}</div>
+			<div className="drop-shadow-[0_0_3px_rgba(0,0,0,0.3)]">
+        {img}
+      </div>
+			<div className="lime-green font-electrolize mt-2 drop-shadow-[0_0_3px_rgba(0,0,0,0.3)]">
+        {title}
+      </div>
+			<div className="text-white text-center font-light text-sm leading-relaxed mt-3 drop-shadow-[0_0_3px_rgba(0,0,0,0.3)]">
+			  {description}
+			</div>
 		</div>
 	);
 }
