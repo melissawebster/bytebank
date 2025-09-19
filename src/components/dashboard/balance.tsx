d612ff5a934a365ed037843d865feda6a0bc4b68
melissawebster <imelissawebster@gmail.com>
2025-09-19 12:27:48 -0300

    fix bug

--- src/components/dashboard/balance.tsx
+++ src/components/dashboard/balance.tsx
@@ -4,7 +4,7 @@ import { darkCardStyle, shinyTitleStyle } from "../shared";
 export default function Balance() {
   return (
     <div className={`${darkCardStyle} relative flex flex-col items-center w-full min-h-[400px]`}>
-      <div className="w-full flex flex-col items-center items-start gap-y-6">
+      <div className="w-full flex flex-col gap-y-6">
         <div className={`text-2xl ${shinyTitleStyle}`}>Balance</div>
       </div>
     </div>
