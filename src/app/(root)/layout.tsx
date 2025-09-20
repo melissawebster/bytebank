0a88ef75c1284e93f66e3ae56b3623130ab6aab8
melissawebster <imelissawebster@gmail.com>
2025-09-20 11:47:53 -0300

    add metadata and og image

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,36 @@
 import Footer from "@/src/components/footer";
 import Header from "@/src/components/header";
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "ByteBank - Experience Greater Financial Freedom",
+  description:
+    "ByteBank is more than a bank — it’s your partner in financial freedom. Open an account today and enjoy tools that fit your lifestyle.",
+  keywords: [
+    "banking",
+    "finance",
+    "checking account",
+    "credit card",
+    "financial freedom",
+  ],
+  openGraph: {
+    title: "ByteBank - Experience Greater Financial Freedom",
+    description:
+      "ByteBank is more than a bank — it’s your partner in financial freedom.",
+    url: "https://bytebank-five.vercel.app/",
+    siteName: "ByteBank",
+    images: [
+      {
+        url: "/assets/bytebank-og.png", 
+        width: 1200,
+        height: 630,
+        alt: "ByteBank promotional card",
+      },
+    ],
+    type: "website",
+  },
+};
 
 export default function RootLayout({
   children,
