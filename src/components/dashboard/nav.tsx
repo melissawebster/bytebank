8c9bddd851078f120498330c4ed03bd746a01671
melissawebster <imelissawebster@gmail.com>
2025-09-23 18:56:39 -0300

    nav styling changes

--- src/components/dashboard/nav.tsx
+++ src/components/dashboard/nav.tsx
@@ -8,11 +8,23 @@ interface NavDashboardProps {
 
 export default function NavDashboard({ className }: NavDashboardProps) {
   return (
-    <div className={twMerge(`dark-card-style text-lg w-full rounded-lg px-4 pt-9 pb-12`, className)}>
-      <div className="w-full flex flex-row lg:flex-col justify-between px-4">
+    <div
+      className={twMerge(
+        `dark-nav-style text-lg w-full rounded-lg p-0`,
+        className
+      )}
+    >
+      <div className="w-full flex flex-row lg:flex-col justify-between">
         {navDashboardItems.map((item, index) => (
-          <Link href={item.url} key={index}>
-            <div className="font-normal lime-green text-sm px-4 py-1 lg:py-4">
+          <Link
+            href={item.url}
+            key={index}
+            className="first:rounded-l-lg last:rounded-r-lg
+                      lg:first:rounded-t-lg lg:first:rounded-l-none
+                      lg:last:rounded-b-lg  lg:last:rounded-r-none
+                      overflow-hidden"
+          >
+            <div className="flex-1 font-normal lime-green text-sm border py-3 lg:py-4 px-4 border-slate-600 bg-slate-800">
               {item.name}
             </div>
           </Link>
